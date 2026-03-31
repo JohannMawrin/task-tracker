c0a0acc70dea0817d36f7ee81d8c8d2ffeca6867
rwaenrd <963852zxc123@gmail.com>
2026-03-31 20:42:18 +0300

    refactor: new files

---
+++ src/task_tracker/manager.py
@@ -0,0 +1,45 @@
+import json
+import logging
+from functools import wraps
+from pathlib import Path
+from typing import Any
+
+logger = logging.getLogger(__name__)
+
+
+def _load_json(file_path: Path) -> Any:
+    if not file_path.exists():
+        return {}
+
+    raw_text = file_path.read_text(encoding="utf-8")
+
+    try:
+        return json.loads(raw_text)
+    except json.JSONDecodeError:
+        logger.exception(f"Failed to decode JSON from: {file_path}")
+        return {}
+
+
+def _save_json(payload: Any, file_path: Path) -> None:
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    serialized = json.dumps(payload, ensure_ascii=False, indent=2)
+    file_path.write_text(serialized, encoding="utf-8")
+
+
+def manage_json(file_path: Path, *, save: bool = False):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            payload = _load_json(file_path)
+
+            revised = func(payload, *args, **kwargs)
+
+            if save:
+                _save_json(revised, file_path)
+
+            return revised
+
+        return wrapper
+
+    return decorator
--- src/task_tracker/services.py
+++ src/task_tracker/services.py
@@ -1,54 +1,12 @@
-import json
-import logging
 from datetime import UTC, datetime
 from enum import StrEnum
-from functools import wraps
 from pathlib import Path
-from typing import Any
+
+from task_tracker.manager import manage_json
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 STORAGE_PATH = BASE_DIR / "data" / "storage.json"
-
-logger = logging.getLogger(__name__)
-
-
-def _load_json(file_path: Path) -> Any:
-    if not file_path.exists():
-        return {}
-
-    raw_text = file_path.read_text(encoding="utf-8")
-
-    try:
-        return json.loads(raw_text)
-    except json.JSONDecodeError:
-        logger.exception(f"Failed to decode JSON from: {file_path}")
-        return {}
-
-
-def _save_json(payload: Any, file_path: Path) -> None:
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-
-    serialized = json.dumps(payload, ensure_ascii=False, indent=2)
-    file_path.write_text(serialized, encoding="utf-8")
-
-
-def _manage_json(file_path: Path, *, save: bool = False):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            payload = _load_json(file_path)
-
-            revised = func(payload, *args, **kwargs)
-
-            if save:
-                _save_json(revised, file_path)
-
-            return revised
-
-        return wrapper
-
-    return decorator
 
 
 class TaskStatus(StrEnum):
@@ -57,7 +15,7 @@ class TaskStatus(StrEnum):
     DONE = "done"
 
 
-@_manage_json(STORAGE_PATH, save=True)
+@manage_json(STORAGE_PATH, save=True)
 def add_task(tasks: dict, description: str) -> dict:
     last_id = max((int(task_id) for task_id in tasks), default=0)
     new_id = str(last_id + 1)
@@ -73,19 +31,19 @@ def add_task(tasks: dict, description: str) -> dict:
     return tasks
 
 
-@_manage_json(STORAGE_PATH)
+@manage_json(STORAGE_PATH)
 def read_tasks(tasks: dict) -> dict:
     return tasks
 
 
-@_manage_json(STORAGE_PATH)
+@manage_json(STORAGE_PATH)
 def filter_tasks_by_status(tasks: dict, status: TaskStatus) -> dict:
     return {
         task_id: task for task_id, task in tasks.items() if task["status"] == status
     }
 
 
-@_manage_json(STORAGE_PATH, save=True)
+@manage_json(STORAGE_PATH, save=True)
 def update_task(
     tasks: dict,
     task_id: int,
@@ -111,7 +69,7 @@ def update_task(
     return tasks
 
 
-@_manage_json(STORAGE_PATH, save=True)
+@manage_json(STORAGE_PATH, save=True)
 def delete_task(tasks: dict, task_id: int) -> dict:
     str_id = str(task_id)
     if str_id not in tasks:
