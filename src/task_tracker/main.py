bdd2a083c579142375500b9ed1449985d3927485
rwaenrd <963852zxc123@gmail.com>
2026-03-31 20:22:21 +0300

    feat: add update_task func

--- src/task_tracker/main.py
+++ src/task_tracker/main.py
@@ -83,3 +83,29 @@ def filter_tasks_by_status(tasks: dict, status: TaskStatus) -> dict:
     return {
         task_id: task for task_id, task in tasks.items() if task["status"] == status
     }
+
+
+@_manage_json(STORAGE_PATH, save=True)
+def update_task(
+    tasks: dict,
+    task_id: int,
+    description: str | None = None,
+    status: TaskStatus | None = None,
+) -> dict:
+    str_id = str(task_id)
+    if str_id not in tasks:
+        raise KeyError(f"Task with id {task_id} not found")
+
+    update_data = {}
+    if description is not None:
+        update_data["description"] = description
+    if status is not None:
+        update_data["status"] = status
+
+    if not update_data:
+        raise ValueError("No fields provided for update")
+
+    update_data["updated_at"] = datetime.now(UTC).isoformat()
+    tasks[str_id].update(update_data)
+
+    return tasks
